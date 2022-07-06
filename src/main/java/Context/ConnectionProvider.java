1a19369884d396a2100e48e5d181b7872e09f43b
baonguyen190301 <69332128+baonguyen190301@users.noreply.github.com>
2022-07-06 20:56:04 +0700

    create project

---
+++ src/main/java/Context/ConnectionProvider.java
@@ -0,0 +1,19 @@
+package Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+private static Connection connection = null;
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		if(connection == null)
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testJava","root","123456789");
+			System.out.print("cennected");
+		}
+		return connection;
+	}
+}
